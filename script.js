ced1a3983095450590629856fc328f538d61c9d0
Syimyk Zaiyrbekov <zaiyrbekovsyimyk@gmail.com>
2022-02-11 14:31:47 +0600

    added img file

---
+++ script.js
@@ -0,0 +1,7 @@
+let example1 = document.querySelector('#example1');
+example1.addEventListener('click', function(event) {
+window.location.href = "https://www.bing.com/"
+
+
+  event.preventDefault(); 
+});
